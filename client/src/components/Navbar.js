f04c3d8287ecb7360aa2cd073397c3ac6ce569b6
YSK-06 <kadam.yash@northeastern.edu>
2025-06-02 17:25:48 -0500

    Updated Navbar and Title

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,22 @@
-import { Navbar as BootstrapNavbar, Nav } from 'bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+function DashboardNavbar() {
   return (
-    <BootstrapNavbar bg="dark" variant="dark" expand="lg">
-      <BootstrapNavbar.Brand href="/">Student Dashboard</BootstrapNavbar.Brand>
-      <BootstrapNavbar.Toggle />
-      <BootstrapNavbar.Collapse>
+    <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
+      <Navbar.Brand as={Link} to="/">Student Dashboard</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
         <Nav className="me-auto">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/courses">Courses</Nav.Link>
-          <Nav.Link as={Link} to="/assignments">Assignments</Nav.Link>
-          <Nav.Link as={Link} to="/grades">Grades</Nav.Link>
-          <Nav.Link as={Link} to="/messages">Messages</Nav.Link>
+          <Nav.Link as={Link} to="/" className="mx-2">Home</Nav.Link>
+          <Nav.Link as={Link} to="/courses" className="mx-2">Courses</Nav.Link>
+          <Nav.Link as={Link} to="/assignments" className="mx-2">Assignments</Nav.Link>
+          <Nav.Link as={Link} to="/grades" className="mx-2">Grades</Nav.Link>
+          <Nav.Link as={Link} to="/messages" className="mx-2">Messages</Nav.Link>
         </Nav>
-      </BootstrapNavbar.Collapse>
-    </BootstrapNavbar>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
-export default Navbar;
+
+export default DashboardNavbar;
